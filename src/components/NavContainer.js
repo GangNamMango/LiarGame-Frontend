8f8122a105f026d8ef5801cb36ea74692876cd0a
SeungWon0921 <hyumapr1@gmail.com>
2022-03-31 23:13:21 +0900

    메인 작업

---
+++ src/components/NavContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './NavContainer.css';
+const NavContainer = () => {
+    return(
+        <div className="Nav_area">
+            <div className="Nav">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+
+export default NavContainer;
---
+++ src/data/character.js
@@ -0,0 +1,35 @@
+const CharaterImg = [
+    {
+        "id" : 1,
+        "image" : "/img/free-icon-alien-4207576.png"
+    },
+    {
+        "id" : 2,
+        "image" : "/img/free-icon-pumpkin-4207358.png"
+    },{
+        "id" : 3,
+        "image" : "/img/free-icon-scientist-4207345.png"
+    },{
+        "id" : 4,
+        "image" : "/img/free-icon-werewolf-4207350.png"
+    },
+    {
+        "id" : 1,
+        "image" : "/img/free-icon-alien-4207576.png"
+    },
+    {
+        "id" : 1,
+        "image" : "/img/free-icon-alien-4207576.png"
+    },
+    {
+        "id" : 1,
+        "image" : "/img/free-icon-alien-4207576.png"
+    },
+    {
+        "id" : 1,
+        "image" : "/img/free-icon-alien-4207576.png"
+    }
+
+]
+
+export default CharaterImg;
