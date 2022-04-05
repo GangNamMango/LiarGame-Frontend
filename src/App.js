1268c0382295c7906b71a3903280e89b9e0c5ebd
ssun-aa <ssk6200@daum.net>
2022-04-06 00:03:10 +0900

    set routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import WaitingRoomPage from "./pages/WaitingRoomPage";
 
 function App() {
-  return <WaitingRoomPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/wait" element={<WaitingRoomPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
