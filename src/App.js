49250cc0b917e5722b832dbf3de025bc7091d632
ssun-aa <ssk6200@daum.net>
2022-04-06 23:27:44 +0900

    set router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import MainPages from "./pages/MainPages";
 import WaitingRoomPage from "./pages/WaitingRoomPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/wait" element={<WaitingRoomPage />} />
+        <Route path="/" element={<MainPages />} />
+        <Route path="/room" element={<WaitingRoomPage />} />
       </Routes>
     </Router>
   );
