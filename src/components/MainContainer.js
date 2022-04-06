9ed6d361db4e08bc7d4a1083f78bc57b1899dbbc
ssun-aa <ssk6200@daum.net>
2022-04-06 23:26:50 +0900

    add mainpages

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,116 +1,93 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./MainContainer.css";
+import CharaterImg from "../data/character";
 import styled from "styled-components";
-import Button from "./Button";
-import { HiOutlineMenuAlt1 } from "react-icons/hi";
-
-const Main = styled.div`
-  position: relative;
-  width: 100vh;
-  max-width: 390px;
-  height: 100%;
-  background: #0f0c13;
-  padding: 0 10%;
-`;
-
-const Menu = styled.div`
-  position: absolute;
-  width: 50px;
-  height: 50px;
-  left: 33px;
-  top: 41px;
-
-  background: #201651;
-  border: 1px solid rgba(0, 0, 0, 0.5);
-  box-sizing: border-box;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-`;
-
-const Character = styled.img`
-  position: absolute;
-  width: 250px;
-  height: 250px;
-  left: 70px;
-  top: 224px;
-
-  background-size: contain;
-  border: 4px solid #54b5c2;
-  box-sizing: border-box;
-  border-radius: 50px;
-
-  cursor: pointer;
-`;
-
-const Form = styled.form`
-  position: absolute;
-  width: 250px;
-  height: 46px;
-  left: 70px;
-  top: 509px;
-
-  background: rgba(0, 0, 0, 0.45);
-  border: 2px solid #6e6693;
-  box-sizing: border-box;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  // filter: blur(4px);
-
-  border-radius: 10px;
-`;
-
-const NickName = styled.input`
-  position: absolute;
-  width: 234px;
-  height: 18px;
-  left: 3px;
-  top: 10px;
-
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 300;
-  font-size: 20px;
-  line-height: 28px;
-
-  color: #757575;
-  background-color: transparent;
-
-  border: none;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-const ButtonGroup = styled.div`
-  position: absolute;
-  left: 97px;
-  top: 643px;
-  width: 195px;
-`;
+import axios from "axios";
 
 const MainContainer = () => {
-  const [img, setImg] = useState(4207345);
+  const [nickName, setNickName] = useState("");
+  const [open, setOpen] = useState(false);
+  const [Join, setJoin] = useState(false);
+  const [getImg, setGetImg] = useState(4207576);
+
+  const changeImg = (i) => {
+    setGetImg(i);
+  };
+
+  const axiosRoom = async () => {
+    try {
+      const API_DOMAIN = "http://3.35.178.104";
+      const response = await axios.post(`${API_DOMAIN}/game/room`, {
+        nickname: nickName,
+        character: getImg,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const onChange = (e) => {
+    setNickName(e.target.value);
+  };
+  const enterRoom = async () => {
+    await axiosRoom();
+  };
   return (
-    <Main>
-      <Menu>
-        <HiOutlineMenuAlt1
-          style={{
-            position: "absolute",
-            top: "5px",
-            right: "5px",
-            color: "#54B5C2",
-            fontSize: "2.5em",
-          }}
-        />
-      </Menu>
-      <Character src={`/img/character-${img}.png`}></Character>
-      <Form>
-        <NickName />
-      </Form>
-      <ButtonGroup>
-        <Button value="방 만들기"></Button>
-        <Button value="참여하기"></Button>
-      </ButtonGroup>
-    </Main>
+    <div className="Wrap">
+      <div className="Container">
+        <div className="Img_area" open={open} onClick={() => setOpen(!open)}>
+          <img src={"/img/character-" + getImg + ".png"} />
+        </div>
+        <div className={Join ? "Join" : "hidden"}>
+          <h1 className="title Blue">방코드</h1>
+          <input className="code_text" type="text" maxLength={10} />
+          <p className="Blue" open={Join} onClick={() => setJoin(!Join)}>
+            입장하기
+          </p>
+        </div>
+        <div className={open ? "Cha_select" : "hidden"}>
+          <div className="title">캐릭터 설정</div>
+          <div className="img_select">
+            {CharaterImg &&
+              CharaterImg.map((item) => (
+                <ul className="img_ul">
+                  <li onClick={() => changeImg(item.id)}>
+                    <input
+                      type="image"
+                      src={item.image}
+                      onClick={() => setOpen(!open)}
+                    ></input>
+                  </li>
+                </ul>
+              ))}
+          </div>
+        </div>
+        <div className="text_box">
+          <input
+            id="Nick_Name"
+            type="text"
+            name="nickname"
+            maxLength={10}
+            value={nickName}
+            onChange={onChange}
+          />
+        </div>
+        <div className="Btn">
+          <ul>
+            <li>
+              <Link to={"/room"}>
+                <span className="Blue" onClick={enterRoom}>
+                  방만들기
+                </span>
+              </Link>
+            </li>
+            <li open={Join} onClick={() => setJoin(!Join)}>
+              참여하기
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ src/pages/MainPages.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MainContainer from "../components/MainContainer";
+import NavContainer from "../components/NavContainer";
+import GlobalStyle from "../GlobalStyles";
+
+const MainPage = () => {
+  return (
+    <>
+      <NavContainer />
+      <MainContainer />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default MainPage;
