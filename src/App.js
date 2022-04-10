f2ba7a14cbbca7d727489fc88f49e98a74133405
ssun-aa <ssk6200@daum.net>
2022-04-10 22:37:44 +0900

    fix router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import MainPages from "./pages/MainPages";
 import WaitingRoomPage from "./pages/WaitingRoomPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPages />} />
-        <Route path="/room" element={<WaitingRoomPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <Route path="/" component={MainPages} exact={true} />
+      <Route path="/room" component={WaitingRoomPage} />
+    </div>
   );
 }
 
